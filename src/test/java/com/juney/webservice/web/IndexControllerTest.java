796bf817710bbe1fad679066be36f5b1cb48905d
juney <juneyj114@gmail.com>
2020-01-13 20:29:31 +0900

    v1.1

--- src/test/java/com/juney/webservice/web/IndexControllerTest.java
+++ src/test/java/com/juney/webservice/web/IndexControllerTest.java
@@ -21,7 +21,7 @@ public class IndexControllerTest {
 	public void loadMainPage() {
 		String body = this.restTemplate.getForObject("/", String.class);
 		
-		assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+		assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
 	}
 
 }
